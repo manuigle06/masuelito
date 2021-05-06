093c8edffce7954571f66ffc53a399296dc735ce
manuigle06 <77737901+manuigle06@users.noreply.github.com>
2021-05-06 20:52:50 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio de sesión</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <form method="post">
+    	<h1>Inicio de sesión</h1>
+    	<input type="text" name="name" placeholder="Nombre completo">
+    	<input type="email" name="email" placeholder="Email">
+    	<input type="submit" name="Iniciar sesión">
+    </form>
+        <?php 
+        include("registrar.php");
+        ?>
+</body>
+</html>
+© 2021 Debategle, Inc.
